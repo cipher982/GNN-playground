01c15d218dd42c89a819f55c960c71e18a2eac45
David Rose <david010@gmail.com>
2021-09-11 19:13:28 -0400

    update gpu setup script

--- code/setup.sh
+++ code/setup.sh
@@ -2,6 +2,22 @@
 wget https://developer.download.nvidia.com/compute/cuda/11.4.2/local_installers/cuda_11.4.2_470.57.02_linux.run
 sudo sh cuda_11.4.2_470.57.02_linux.run
 
+# Conda installs
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/anaconda.sh
+bash ~/anaconda.sh -b -p $HOME/anaconda
+echo -e '\nexport PATH=$HOME/anaconda/bin:$PATH' >> $HOME/.bashrc && source $HOME/.bashrc
+conda install pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch -c nvidia
+conda install pytorch-geometric -c rusty1s -c conda-forge
+git clone https://github.com/cipher982/GNN-playground.git
+cd ..
+conda install -c anaconda ipykernel
+conda install pyarrow
+conda install -y s3fs
+
+python -m ipykernel install --user --name=torch
+
+exec jupyter lab --no-browser --port=8888 --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password=''
+
 # install torch
 pip3 install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
 
