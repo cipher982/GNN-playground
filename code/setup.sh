6ba3c3e6ba33ed4144ba9ec5733e541c831a28b5
David Rose <david010@gmail.com>
2021-09-12 20:07:07 -0400

    update setup script

--- code/setup.sh
+++ code/setup.sh
@@ -6,26 +6,19 @@ sudo sh cuda_11.4.2_470.57.02_linux.run
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/anaconda.sh
 bash ~/anaconda.sh -b -p $HOME/anaconda
 echo -e '\nexport PATH=$HOME/anaconda/bin:$PATH' >> $HOME/.bashrc && source $HOME/.bashrc
-conda install pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch -c nvidia
-conda install pytorch-geometric -c rusty1s -c conda-forge
-git clone https://github.com/cipher982/GNN-playground.git
-cd ..
-conda install -c anaconda ipykernel
-conda install pyarrow
-conda install -y s3fs
-
-python -m ipykernel install --user --name=torch
-
-exec jupyter lab --no-browser --port=8888 --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password=''
-
-# install torch
-pip3 install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
-
-# install geometric
-pip3 install torch-scatter -f https://data.pyg.org/whl/torch-1.9.0+${CUDA}.html
-pip3 install torch-sparse -f https://data.pyg.org/whl/torch-1.9.0+${CUDA}.html
-pip3 install torch-geometric
+conda install -y pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch -c nvidia
+conda install -y pytorch-geometric -c rusty1s -c conda-forge
+conda install -y ipykernel pyarrow s3fs matplotlib
+pip install pytorch_lightning
 
 # install dgmc
 git clone https://github.com/rusty1s/deep-graph-matching-consensus.git
-cd deep-graph-matching-consensus && python3 setup.py install
+cd deep-graph-matching-consensus && python setup.py install
+
+python -m ipykernel install --user --name=torch
+
+exec jupyter lab --no-browser --port=8889 --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password=''
+
+# install torch
+#pip3 install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+
